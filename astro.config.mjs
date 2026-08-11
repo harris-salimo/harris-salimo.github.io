d2ca8aefe677c0be3bb89bcbd1f6e8e407e79c24
Harris Salimo <harrismailasalimo@gmail.com>
2026-08-12 00:48:35 +0300

    feat: add sitemap and robots metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://harris-salimo.github.io',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   devToolbar: {
     enabled: false,
   },
