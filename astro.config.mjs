90a6fe38aa472939ddad6fefe60642db7f2faac6
Harris Salimo <harrismailasalimo@gmail.com>
2026-08-11 22:30:45 +0300

    feat: refine hero and deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://harris-salimo.github.io',
   devToolbar: {
     enabled: false,
   },
