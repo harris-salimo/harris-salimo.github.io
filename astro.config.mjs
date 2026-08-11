53a112fd3d6a98ee89aa17514e8dfaa473384106
Harris Salimo <harrismailasalimo@gmail.com>
2026-08-11 22:48:19 +0300

    fix: configure base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://harris-salimo.github.io',
+  base: '/harris-salimo.github.io',
   devToolbar: {
     enabled: false,
   },
